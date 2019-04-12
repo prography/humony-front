197c33aa7d9e843e22243c3221c1d56345c09cf3
sanghup1234 <sanghup123@naver.com>
2019-04-12 21:45:36 +0900

    scroll에 따른 header 스타일 적용

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,16 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../styles/reset.scss';
 import PageTemplate from './common/PageTemplate';
 import Main from './Main';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <PageTemplate>
-        <Main />
-      </PageTemplate>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <PageTemplate>
+          <Main />
+        </PageTemplate>
+      </div>
+    );
+  }
 };
 
 export default App;
--- src/components/common/PageTemplate/PageTemplate.tsx
+++ src/components/common/PageTemplate/PageTemplate.tsx
@@ -1,17 +1,50 @@
-import React from 'react';
+import React, { ReactNode, Component } from 'react';
 import Header from '../Header';
 import Footer from '../Footer';
 
-const PageTemplate: React.FC = ({ children }) => {
-    return (
-        <div className="page-template">
-            <Header />
-            <main>
-                {children}
-            </main>
-            <Footer />
-        </div>
-    );
-};
+interface Props {
+    children: ReactNode
+}
+
+interface State {
+    scrollTop: number
+}
+
+
+class PageTemplate extends Component<Props, State> {
+    state = {
+        scrollTop: 0
+    };
+    componentDidMount () {
+        window.addEventListener('scroll', this.WindowScroll);
+    }
+
+    shouldComponentUpdate (nextProps: Props, nextState: State): any {
+        if (this.state.scrollTop <= 100 && nextState.scrollTop >= 100 || this.state.scrollTop >= 100 && nextState.scrollTop <= 100) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    WindowScroll  = () => {
+        this.setState({
+            scrollTop: document.documentElement.scrollTop
+        });
+    }
+
+    render () {
+        console.log('123');
+        return (
+            <div className="page-template">
+             <Header scrollTop ={this.state.scrollTop} />
+             <main>
+                 {this.props.children}
+             </main>
+             <Footer />
+         </div>
+        );
+    }
+}
 
 export default PageTemplate;
