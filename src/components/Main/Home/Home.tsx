b5309212959b24fd3ba141614b0a5a8733612661
Alvin <sanghup123@naver.com>
2019-04-15 21:24:17 +0900

    file select

--- src/components/Main/Home/Home.tsx
+++ src/components/Main/Home/Home.tsx
@@ -1,22 +1,56 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Home.scss';
+//import console = require('console');
 
-const Home: React.FC = () => {
-    return (
-        <section id="home">
-            <div className="uploadBox">
-                <h2>파일 업로드</h2>
-                <form>
-                    <input type="file" name="" id="" />
-                    <input type="submit" />
-                </form>
-            </div>
-            <div className="titleInfo">
-                <h3>사진을 쉽고 빠르게 자르자! <br/>윤대영쌤의 딥러닝과 함께하는<br/>재미있는 누끼따기</h3>
-                <button className="startBtn"><span>START</span> 지금 바로 시작하기</button>  
-            </div>
-        </section>
-    );
+
+interface Props {
+    
 };
+
+interface State {
+    selectedFile: string
+};
+
+class Home extends Component<Props, State> {
+    state = {
+        selectedFile: ''
+    };
+
+    handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const file: any = e.target.files;
+   
+        if (file.length) {
+            this.setState({
+                selectedFile: file[0].name
+            });
+        }
+        
+    }
+
+    render () {
+        return (
+            <section id="home">
+                <div className="home-wrap">
+                    <div className="uploadBox">
+                        <h2>파일 업로드</h2>
+                        <form>
+                            <div className="filebox">
+                                <label htmlFor="ex_file">
+                                    파일을 선택해 주세요
+                                </label>
+                                <input type="file" onChange={this.handleChange} id="ex_file" />
+                            </div>
+                            <p className="fileName">{this.state.selectedFile}</p>
+                        </form>
+                    </div>
+                    <div className="titleInfo">
+                        <h3><span className="title-concept">사진을 쉽고 빠르게 자르자!</span> <br/>윤대영쌤의 딥러닝과 함께하는<br/>재미있는 누끼따기</h3>
+                        <button className="startBtn"><span>START</span> 지금 바로 시작하기</button>  
+                    </div>
+                </div>
+            </section>
+        );
+    }
+}
 
 export default Home;
