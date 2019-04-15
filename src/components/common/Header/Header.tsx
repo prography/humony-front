ef649bfa64421b7ea9a15ef0d71523e117e03e13
Alvin <sanghup123@naver.com>
2019-04-15 21:23:39 +0900

    mobile menu add

--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Header.scss';
 import styled from 'styled-components';
 
@@ -6,25 +6,47 @@ interface Props {
     scrollTop: number
 }
 
-const Header: React.FC<Props> = ({ scrollTop }) => {
-    return (
-        <header className={ scrollTop >= 100 ? 'header-fix' : ''}>
-            <nav>
-                <div className="header-container">
-                    <div className="logo">
-                        <h1>
-                            <a href="/">Picxy</a>
-                        </h1>
+interface State {
+    mobileOpen: boolean
+}
+
+
+class Header extends Component<Props, State> {
+    state = {
+        mobileOpen: false
+    }
+
+    handleClick = () => {
+        this.setState({
+            mobileOpen: !this.state.mobileOpen
+        });
+    }
+
+    render () {
+        return (
+            <header className={ this.props.scrollTop >= 100 ? 'header-fix' : ''}>
+                <nav>
+                    <div className="header-container">
+                        <div className="logo">
+                            <h1>
+                                <a href="/">Picxy</a>
+                            </h1>
+                        </div>
+                        <ul className={`gnb_list ${this.state.mobileOpen ? 'is_open' : ''}`}>
+                            <li><a href="#">홈</a></li>
+                            <li><a href="#">서비스 소개</a></li>
+                            <li><a href="#">픽시 플러스</a></li>
+                        </ul>
+                        <div className={`mobile-menu ${this.state.mobileOpen ? 'is_open' : ''}`} onClick={this.handleClick}>
+                            <div className="bar"></div>
+                            <div className="bar"></div>
+                            <div className="bar"></div>
+                        </div>
                     </div>
-                    <ul className="gnb_list">
-                        <li><a href="#">홈</a></li>
-                        <li><a href="#">서비스 소개</a></li>
-                        <li><a href="#">픽시 플러스</a></li>
-                    </ul>
-                </div>
-            </nav>
-        </header>
-    );
-};
+                </nav>
+            </header>
+        );
+    }
+}
 
 export default Header;
