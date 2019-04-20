5b83c5f5eb363903e88af662cb5450cbffdb6eaa
sanghup1234 <sanghup123@naver.com>
2019-04-20 16:27:51 +0900

    api url 수정

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8000';
+const API_URL = 'http://localhost:8000/api';
 
 export function sendImage (formData: any) {
     return axios.post(`${API_URL}/inpic`, formData, {
